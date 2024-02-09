193a00a109e437e5ebd26601a04199d4fb359b3b
Jeremy Jefferson <jeremyijefferson@gmail.com>
2024-02-09 14:58:28 -0800

    finished

--- colorFlipper/App.js
+++ colorFlipper/App.js
@@ -1,6 +1,13 @@
-const colors = ["green", "red", "rgba(133,122,200", "#f15025"];
 const btn = document.getElementById("btn");
 const color = document.querySelector(".color");
 
 // this is where the logic for clicking the button would be
-btn.addEventListener("click", () => {});
+btn.addEventListener("click", () => {
+	document.body.style.backgroundColor = getRandomHexCode();
+	color.textContent = getRandomHexCode();
+});
+
+function getRandomHexCode() {
+	let randomColor = Math.floor(Math.random() * 16777215).toString(16);
+	return "#" + "0".repeat(6 - randomColor.length) + randomColor;
+}
