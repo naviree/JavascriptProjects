94fc691a5f5fc3cf36e96b993fd1db7597da73a7
Jeremy Jefferson <jeremyijefferson@gmail.com>
2024-02-09 11:57:08 -0800

    Update code to fix bug in login functionality

---
+++ colorFlipper/App.js
@@ -0,0 +1,6 @@
+const colors = ["green", "red", "rgba(133,122,200", "#f15025"];
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+// this is where the logic for clicking the button would be
+btn.addEventListener("click", () => {});
