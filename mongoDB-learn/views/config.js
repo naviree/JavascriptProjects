d5e314d61b6a41e1e200e2945e1d921c71a0e3e0
Jeremy Jefferson <jeremyijefferson@gmail.com>
2024-02-20 17:20:01 -0800

    Update .DS_Store file

---
+++ mongoDB-learn/views/config.js
@@ -0,0 +1,9 @@
+const config = {
+	db: {
+		username: "admin",
+		connection:
+			"mongodb+srv://admin:1234@jun.wvbznxb.mongodb.net/practice?retryWrites=true&w=majority",
+	},
+};
+
+module.exports = config;
