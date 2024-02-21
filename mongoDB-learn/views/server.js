234a95bae4048ef2a3231230b2d518cac14ee0a6
Jeremy Jefferson <jeremyijefferson@gmail.com>
2024-02-21 12:09:50 -0800

    Add car model and save to MongoDB

--- mongoDB-learn/views/server.js
+++ mongoDB-learn/views/server.js
@@ -4,3 +4,35 @@ const mongoose = require("mongoose");
 mongoose.connect(config.db.connection);
 
 console.log(config.db.connection);
+console.log("Connected to MongoDB");
+
+//
+
+const carSchema = new mongoose.Schema({
+	make: String,
+	model: String,
+	year: Number,
+	color: String,
+	mileage: Number,
+	needsRepair: Boolean,
+});
+
+const Car = mongoose.model("Car", carSchema);
+
+const myCar = new Car({
+	make: "Toyota",
+	model: "Corolla",
+	year: 2007,
+	color: "grey",
+	mileage: 110000,
+	needsRepair: false,
+});
+
+myCar
+	.save()
+	.then((car) => {
+		console.log(car);
+	})
+	.catch((err) => {
+		console.error(err);
+	});
