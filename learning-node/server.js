4a5d5a38c89a6e599a9d488255ff19d54837cc64
Jeremy Jefferson <jeremyijefferson@gmail.com>
2024-02-22 08:30:37 -0800

    Fix MongoDB connection and update server.js

--- learning-node/server.js
+++ learning-node/server.js
@@ -1,20 +1,10 @@
 const express = require("express");
-const mongoose = require("mongoose");
 const app = express();
 
-const uri =
-	"mongodb+srv://jelly:1234@jun.6xgkvde.mongodb.net/test?retryWrites=true&w=majority";
-
-async function connect(uri) {
-	try {
-		await mongoose.connect(uri); // Fix the typo here (use uri instead of url)
-		console.log("connected to MongoDB");
-	} catch (error) {
-		console.log(error);
-	}
-}
-
-connect(uri); // Pass the uri to the connect function
+// Pass the uri to the connect function
+app.get("/", (req, res) => {
+	res.send("Hello, this is your Express server!");
+});
 
 app.listen(8001, () => {
 	console.log("Server started on port 8001");
--- mongoDB-learn/views/server.js
+++ mongoDB-learn/views/server.js
@@ -9,30 +9,46 @@ console.log("Connected to MongoDB");
 //
 
 const carSchema = new mongoose.Schema({
-	make: String,
-	model: String,
-	year: Number,
-	color: String,
-	mileage: Number,
-	needsRepair: Boolean,
+  make: String,
+  model: String,
+  year: Number,
+  color: String,
+  mileage: Number,
+  needsRepair: Boolean,
 });
 
 const Car = mongoose.model("Car", carSchema);
 
 const myCar = new Car({
-	make: "Toyota",
-	model: "Corolla",
-	year: 2007,
-	color: "grey",
-	mileage: 110000,
-	needsRepair: false,
+  make: "Toyota",
+  model: "Corolla",
+  year: 2007,
+  color: "grey",
+  mileage: 110000,
+  needsRepair: false,
 });
 
-myCar
-	.save()
-	.then((car) => {
-		console.log(car);
-	})
-	.catch((err) => {
-		console.error(err);
-	});
+// Car.create(myCar, (err, car) => {
+//   if (err) {
+//     console.error(err);
+//   }
+//   console.log(car);
+// });
+
+// Car.create(myCar)
+//   .then((err, car) => {
+//     if (err) {
+//       console.error(err);
+//     }
+//     console.log(car);
+//   })
+//   .then(console.log(Car));
+
+Car.findById("65d530e9839d0da6c6edbdbb")
+  .exec() // execute the query and return a promise
+  .then((foundCar) => {
+    console.log(foundCar);
+  })
+  .catch((err) => {
+    console.log("err" + err);
+  });
