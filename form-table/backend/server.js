f4c556dafb56cc44cbe346670c53637bb852e217
Jeremy Jefferson <jeremyijefferson@gmail.com>
2024-02-22 13:21:59 -0800

    created server

--- form-table/backend/server.js
+++ form-table/backend/server.js
@@ -1,2 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+
+mongoose.connect(
+  "mongodb+srv://admin:1234@jun.wvbznxb.mongodb.net/name?retryWrites=true&w=majority&appName=jun",
+);
