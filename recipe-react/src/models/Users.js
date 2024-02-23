c62d8879896d3043d67b4e2ed2ee588cb04af5c1
Jeremy Jefferson <jeremyijefferson@gmail.com>
2024-02-23 08:33:32 -0800

    Add user model and route for registration

---
+++ recipe-react/src/models/Users.js
@@ -0,0 +1,8 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+    username: {type: String, require: true, unique: true},
+    password: {type: String, require: true},
+});
+
+export const UserModel = mongoose.model('users', userSchema);
