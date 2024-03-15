5f4800ef164fa18c78560d448315ab6054050063
Jeremy Jefferson <jeremyijefferson@gmail.com>
2024-03-15 13:28:48 -0700

    Add sillyname and superheroes dependencies to package.json

---
+++ backend/2.3 NPM/index.js
@@ -0,0 +1,9 @@
+import generateName from "sillyname";
+import superHeroe from "superheroes"
+let sillyName = generateName();
+let superHeroes = superHeroe.random();
+
+
+console.log("My name is:", sillyName)
+
+console.log("I am", superHeroes)
